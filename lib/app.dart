22bfcf3990eadbb1dcb3f4a7ef5dc7f580e4b715
ProgrammerHasan <mehedihassan8604@gmail.com>
2021-06-01 22:29:20 +0600

    app

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,7 @@ import 'package:flutter_mvc_pattern/routes.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(f
       debugShowCheckedModeBanner: false,
       locale: Locale('en'),
       title: 'ProgrammerHasan',
